125f835ac9ebd6bfd6212ee3516c86ecb0934189
gurban gurbanzade <gurban.gurbanzadeh@gmail.com>
2023-12-20 22:55:17 +0400

    saf

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.get("/", (req, res) => {
   res.send("<h1>Users</h1><br/><h1>Products</h1>");
   //   res.send(arr);
 });
+app.get("/users", (req, res) => {
+  //   res.send("Hello World!");
+  //   res.send("<h1>Hello World!</h1>");
+  res.send("salam");
+  //   res.send(arr);
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
