d7c79c1f39b29e873c4e3b9b6271d46fb8e0c79a
gurban gurbanzade <gurban.gurbanzadeh@gmail.com>
2023-12-20 22:46:27 +0400

    saf

--- src/routers/userRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const userControllers = require("./../controllers/userControllers");
-
-router.post("/", userControllers.postUser);
-router.get("/", userControllers.getAllUser);
-
-module.exports = router;
